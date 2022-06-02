43846e6b5685963094be8b1f52435a6f1d72cbda
Ashley Jiang <ashleyjiang4@gmail.com>
2022-06-02 14:57:41 +0200

    updated inputs for events

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,41 +3,23 @@ import { PostsContext } from '../contexts/PostContext.js';
 
 
 export default function Post({category}) {
-    const {inputValues, setInputValues, address, setAddress} = useContext(PostsContext)
-
-    function convertToBase64(file) {
-        return new Promise((resolve, reject) => {
-          const fileReader = new FileReader();
-          fileReader.readAsDataURL(file);
-          fileReader.onload = () => {
-            resolve(fileReader.result);
-          }
-          fileReader.onerror = (error) => {
-            reject(error);
-          }
-        })
-      }
+    const {inputValues, setInputValues, address, setAddress,
+           handleFileUpload} = useContext(PostsContext)
     
-    const handleFileUpload = async (e) => {
-        const file = e.target.files[0];
-        const base64 = await convertToBase64(file)
-        setInputValues({...inputValues, image: base64})
-    }
-
     function handleSubmit(e) {
         e.preventDefault()
         setInputValues({...inputValues, address})
         const config ={
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(inputValues)
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(inputValues)
         }
+
         fetch(`http://localhost:7000/${category}`, config)
             .then((response) => response.json())
             .then((result) => console.log(category, "fetch result:", result))
             .catch((error) => console.log(error));
     }
-    
 
     return (
         <form className="PostForm" onSubmit={handleSubmit}>
@@ -51,20 +33,41 @@ export default function Post({category}) {
 
             {category === "event" && (
                 <>
-                    <h4>Address</h4>
+                    <h4>Location</h4>
                     <input type="text" name="street" placeholder="street"
                            onChange={e => setAddress({...address, street: e.target.value})}  />
+                    <input type="text" name="streetNumber" placeholder="street number"
+                           onChange={e => setAddress({...address, streetNumber: e.target.value})}  />
                     <input type="text" name="zip" placeholder="zip code"
                            onChange={e => setAddress({...address, zip: parseInt(e.target.value)})}  />
                     <input type="text" name="city" placeholder="city"
                            onChange={e => setAddress({...address, city: e.target.value})}  />
                     <input type="text" name="country" placeholder="country"
                            onChange={e => setAddress({...address, country: e.target.value})}  />
+
+                    <h4>Time</h4>
+                    <input type="date" name="startDate" 
+                           onChange={e => setInputValues({...inputValues, startDate: e.target.value}) } 
+                           min={(new Date()).toISOString().split('T')[0]} 
+                           max={(new Date((new Date()).setFullYear((new Date()).getFullYear() + 1))).toISOString().split('T')[0]}/>
+
+                    <input type="time" name="startTime"
+                           onChange={e => setInputValues({...inputValues, startTime: e.target.value}) } />
+
+                    <input type="date" name="endDate" 
+                           onChange={e => setInputValues({...inputValues, endDate: e.target.value}) } 
+                           min={(new Date()).toISOString().split('T')[0]} 
+                           max={(new Date((new Date()).setFullYear((new Date()).getFullYear() + 1))).toISOString().split('T')[0]} />
+                    
+                    <input type="time" name="endTime"
+                           onChange={e => setInputValues({...inputValues, endTime: e.target.value}) }  / >
                 </>
                 
             )}
-
+            <label for="video">video:</label>
             <input type="file" name="video" accept="video/mp4,video/x-m4v,video/*" />
+
+            <label for="video">image:</label>
             <input type="file" name="image" accept=".jpeg, .png, .jpg"
                    onChange={(e) => handleFileUpload(e) }/>
 
@@ -77,7 +80,7 @@ export default function Post({category}) {
                    onChange={e => {
                     const removeSpaces = e.target.value.replace(/\s+/g, '')
                     const toArray = removeSpaces.split(',')
-                    setPostData({ ...inputValues, tags: toArray })}} />
+                    setInputValues({ ...inputValues, tags: toArray })}} />
 
             <button type="submit">Create</button>
         </form>
--- src/contexts/PostContext.js
+++ src/contexts/PostContext.js
@@ -5,13 +5,45 @@ export const PostsContext = React.createContext(null)
 function PostsContextProvider({ children }){
     const postCategories = ["beauty", "artsCraft", "garden", "recipe", "event"] 
     const [inputValues, setInputValues] = useState({ title: "", description: "", link: "", tags: "" })
-    const [address, setAddress] = useState({ street: "", zip: "", city: "", country: ""})
+    const [address, setAddress] = useState({ street: "", streetNumber: "", zip: "", city: "", country: ""})
 
+    function convertToBase64(file) {
+        return new Promise((resolve, reject) => {
+          const fileReader = new FileReader();
+          fileReader.readAsDataURL(file);
+          fileReader.onload = () => {
+            resolve(fileReader.result);
+          }
+          fileReader.onerror = (error) => {
+            reject(error);
+          }
+        })
+      }
+    
+    const handleFileUpload = async (e) => {
+        const file = e.target.files[0];
+        const base64 = await convertToBase64(file)
+        setInputValues({...inputValues, image: base64})
+    }
+
+    function handleDelete() {
+        const config ={
+            method: "DELETE",
+            // headers: { Authorization: 'Bearer ' + "token"}, this is automated with cookies
+            body: JSON.stringify(inputValues)
+        }
+    
+        fetch(`http://localhost:7000/${category}`, config)
+            .then((response) => response.json())
+            .then((result) => console.log(category, "fetch result:", result))
+            .catch((error) => console.log(error));
+    }
 
     const postsContextValue = {
         postCategories,
         inputValues, setInputValues,
-        address, setAddress
+        address, setAddress,
+        convertToBase64, handleFileUpload,
     }
 
     return (
