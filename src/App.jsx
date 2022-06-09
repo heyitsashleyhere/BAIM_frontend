4aa1a4679417b00bd583302d9bcc9952a0af3d69
Ivo Serra <dci.ivoserra@gmail.com>
2022-06-09 15:09:02 +0200

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,60 @@
-import Posts from "./components/Posts.jsx";
-import UserLogin from "./components/UserLogin.jsx";
-import UserRegistration from "./components/UserRegistration.jsx";
-import UserEdit from "./components/UserEdit.jsx";
-import UserDelete from "./components/UserDelete.jsx";
-import UserLogout from "./components/UserLogout.jsx";
-
+// import Posts from "./components/Posts.jsx";
+// import UserLogin from "./components/UserLogin.jsx";
+// import UserRegistration from "./components/UserRegistration.jsx";
+// import UserEdit from "./components/UserEdit.jsx";
+// import UserDelete from "./components/UserDelete.jsx";
+// import UserLogout from "./components/UserLogout.jsx";
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom'
+import { Allrecipes } from './components/Private/Allrecipes/Allrecipes.jsx';
+import { AppHeader } from './components/Private/Appheader/AppHeader.jsx';
+import Header from "./components/Public/header/Header.jsx";
+import { Recipes } from './Pages/Private/Recipes/Recipes.jsx';
 
 export default function App() {
+
+  const [ isLogin, setIsLogin]=useState(true)
+
   return (
-    <div>
-      <UserRegistration />
-      <UserLogin />
-      <UserLogout />
-      <UserEdit />
-      <UserDelete />
-      <Posts />
-    </div>
+    <>
+   {isLogin ? <AppHeader/> : <Header/>}
+    
+    <Routes>
+     
+      <Route path="/" element={<p>main</p>}/>
+      <Route path="/about" element={<p>about</p>}/>
+      <Route path="/team" element={<p>team</p>}/>
+      <Route path="/login" element={<p>login</p>}/>
+
+      {isLogin && <>
+
+      <Route path="/discover">
+      <Route index element={<h1>DISCOVER</h1>}/>
+      <Route path="/discover/recipes">
+        <Route index element={<Allrecipes/>}/>
+        {/* <Route path="/discover/recipes/:title" element={<Recipes/>}/> */}
+      </Route>
+      <Route path="/discover/gardens">
+        <Route index element={<p>gardens</p>}/>
+      </Route>
+      <Route path="/discover/beauty">
+        <Route index element={<p>gardens</p>}/>
+      </Route>
+
+      </Route>
+
+      <Route path="/feed" element={<h1>feed</h1>}/>
+      <Route path="/profile" element={<h1>profile</h1>}/>
+      <Route path="/create" element={<h1>create</h1>}/>
+      <Route path="/logout" element={<h1>logout</h1>}/>
+
+      </> }
+
+      <Route path="*" element={<p>error</p>}/>
+
+    </Routes>
+
+    </>
+
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import { createRoot } from "react-dom/client";
+import { createRoot} from "react-dom/client";
+import {BrowserRouter} from 'react-router-dom'
 import App from "./App.jsx";
 import PostsContextProvider from "./contexts/PostContext.js";
 import { UserContextProvider } from "./contexts/UserContext.js";
+import About from '../src/Pages/Public/About/About.jsx'
 
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(
+  <BrowserRouter>
   <UserContextProvider>
     <PostsContextProvider>
       <App />
     </PostsContextProvider>
   </UserContextProvider>
+   </BrowserRouter>
 );
