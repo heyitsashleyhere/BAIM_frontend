deef1d8e3d007b1f956a30dff69d1fd60e16341b
Ivo Serra <dci.ivoserra@gmail.com>
2022-06-13 09:46:51 +0200

    avatar

---
+++ src/components/Private/Avatars/ProfileAvatar.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+
+
+export const ProfileAvatar = ({name, image}) => {
+
+
+  return (
+
+    <section>
+         <img src={image}></img>
+
+    </section>
+    
+  )
+}
