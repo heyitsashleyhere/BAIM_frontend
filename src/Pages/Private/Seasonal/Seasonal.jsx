b7bc0a118d7cc0fe1f9f62ce8764c3aef683dc4c
Ivo Serra <dci.ivoserra@gmail.com>
2022-06-14 16:07:06 +0200

    added pages

---
+++ src/Pages/Private/Seasonal/Seasonal.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Seasonal = () => {
+  return (
+    <section className="Pages">
+        <section className="Hero">
+            <section className="hero-image seasonal">
+            
+            </section>
+        
+            <section className="Hero-text">
+                <h1>Seasonal</h1>
+                <p>Harvest calendar</p>
+            </section>
+
+        </section>
+    </section>
+  )
+}
