17119f427bf95124ac5ede81e1373c28237b43f7
Ashley Jiang <ashleyjiang4@gmail.com>
2022-07-11 22:48:32 +0200

    remove NONEED files

--- src/components/FORASHLEY-BACKEND.js
+++
@@ -1,5 +0,0 @@
-
-
-//or 
-
-const softPinkImage = "https://firebasestorage.googleapis.com/v0/b/baimimages.appspot.com/o/files%2Fimage%2FLokaWhite_forPosts.png?alt=media&token=006943af-6798-4067-aca2-ce0de5771391"
