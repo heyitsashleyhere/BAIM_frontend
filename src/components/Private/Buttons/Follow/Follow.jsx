e795dc29dc81bbbe309ef0f4dbf57a3f8d7570b7
Ashley Jiang <ashleyjiang4@gmail.com>
2022-07-20 11:21:41 +0200

    update follow

--- src/components/Private/Buttons/Follow/Follow.jsx
+++ src/components/Private/Buttons/Follow/Follow.jsx
@@ -75,7 +75,7 @@ export const Follow = ({ name }) => {
 
 	return (
 		<>
-			{profileData && !isAuthor && (
+			{followers && !isAuthor && (
 				<Button
 					style={{ fontSize: "1rem", padding: '0.5em 1em' }}
 					variant={
@@ -91,7 +91,7 @@ export const Follow = ({ name }) => {
 				</Button>
 			)}
 			<p>{error}</p>
-			{profileData && (
+			{followers && (
 				<Modal open={isModalOpen} onClose={() => setIsModalOpen(false)}>
 					<Snackbar open={isModalOpen} autoHideDuration={6000}
 						onClose={handleClose}
